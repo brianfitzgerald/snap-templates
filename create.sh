b5dc8c6ad3a789114f17926ac7612f375a810690
Brian Fitzgerald <BrianFitzgerald@carfax.com>
2018-04-09 17:11:28 -0500

    add readme

--- create.sh
+++
@@ -1,6 +0,0 @@
-npm init
-mkdir src
-mkdir build
-npm i --save-dev concurrently typescript @types/node @types/express express axios nodemon
-tsc —init
-touch src/index.ts
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Resolver Mapping Templates
+
+A template syntax for easily integrating various services with GraphQL in Express.
+
+## Getting Started
+
+Coming soon.
