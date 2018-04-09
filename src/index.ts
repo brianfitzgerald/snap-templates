7af2f5d961333526066844ca15921959f3ce4e9e
Brian Fitzgerald <BrianFitzgerald@carfax.com>
2018-04-09 14:55:48 -0500

    add buildResolver

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,20 @@ import * as graphqlHTTP from "express-graphql"
 import { DynamoDB } from "aws-sdk"
 import { AttributeValue } from "aws-sdk/clients/dynamodb"
 
+type MappingConfiguration = {
+  [key: string]: ResolverMappingTemplate
+}
+
 type DynamoMappingTemplate = {
+  kind: "DynamoDB"
   operation: "GetItem" | "PutItem"
   table: string
   key: { [key: string]: AttributeValue }
   consistentRead: boolean
 }
-type LambdaMappingTemplate = {}
+type LambdaMappingTemplate = {
+  kind: "Lambda"
+}
 
 type ResolverMappingTemplate = DynamoMappingTemplate | LambdaMappingTemplate
 
@@ -18,19 +25,12 @@ export const APPLICATION_PORT = 3000
 
 const app = express()
 
-const resolvers = {
-  dynamo: {
-    getItem: (request: DynamoMappingTemplate, response: express.Response) => {
-      const params: DynamoDB.Types.GetItemInput = {
-        TableName: request.table,
-        Key: request.key
-      }
-      const dynamo = new DynamoDB().getItem(params, (err, data) => {
-        response.json(data)
-      })
-    }
-  }
-}
+// get graphql request
+// send to response mapper
+// response mapper send that request to a certain resolver based on a mapping object
+// resolver completes request
+
+// function that takes in the mapping and generates resolvers for em
 
 const schema = buildSchema(`
   type Person {
@@ -57,18 +57,64 @@ const persons = [
   }
 ]
 
-const rootResolver = {
-  person: ({ id }: { id: number }) => {
-    const person = persons.filter(p => p.id === id)[0]
-    return persons.filter(p => p.id === id)[0]
+const mapping: MappingConfiguration = {
+  person: {
+    kind: "DynamoDB",
+    operation: "GetItem",
+    table: "ambliss-songs",
+    consistentRead: false,
+    key: {
+      id: {
+        S: "$context.arguments.id"
+      }
+    }
   }
+}
+
+type Resolvers = {
+  [key: string]: Function
+}
+
+const resolvers: Resolvers = {
+  DynamoDB: (
+    mappingParams: DynamoMappingTemplate,
+    request: any,
+    response: any
+  ) => {
+    console.log("hit resolver")
+    console.log(mappingParams, request)
+
+    if (mappingParams.operation === "GetItem") {
+      const params: DynamoDB.Types.GetItemInput = {
+        TableName: mappingParams.table,
+        Key: mappingParams.key
+      }
+      const dynamo = new DynamoDB().getItem(params, (err, data) => {
+        response.json(data)
+      })
+    }
+  }
+}
+
+const buildResolver = (mappingTemplate: MappingConfiguration) => {
+  const finalMapping: { [key: string]: Function } = {}
+
+  Object.keys(mappingTemplate).forEach(element => {
+    const kind = mappingTemplate[element].kind
+    const resolver = resolvers[kind]
+    finalMapping[kind] = resolver.bind(mappingTemplate)
+  })
+
+  console.log(mappingTemplate)
+
+  return mappingTemplate
 }
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
-    rootValue: rootResolver,
+    rootValue: buildResolver(mapping),
     graphiql: true
   })
 )
